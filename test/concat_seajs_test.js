65be21576a1d9c460bce7fd471926a181bb99a80
zero-g <18959260204@163.com>
2015-08-31 21:27:30 +0800

    整理

--- test/concat_seajs_test.js
+++ test/concat_seajs_test.js
@@ -22,7 +22,7 @@ var grunt = require('grunt');
     test.ifError(value)
 */
 
-exports.concat_seajs = {
+exports.component = {
   setUp: function(done) {
     // setup here if necessary
     done();
